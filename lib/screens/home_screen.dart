990b86baaffae473f5db5f86f24809ab26323b82
EthikalHakingGhost <travo.edward@gmail.com>
2023-08-12 22:28:08 -0400

    minor updates renamed pages

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,8 @@
 // ignore: file_names
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:radio_app_v2/screens/MoreMenus.dart';
-import 'package:radio_app_v2/screens/RadioPlayer.dart';
+import 'package:radio_app_v2/screens/menus.dart';
+import 'package:radio_app_v2/screens/radio_player.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 final Uri fbUrl =
